5441c0e3fdca2bf2be180b604f71c03354cadb54
CamarataM <CamarataM@cwu.edu>
2023-05-23 13:55:26 -0700

    Cleanup and fully implement stegoDestroy.c
    The code is much more readable now and features two methods of stego destruction. The first will attempt to repair the file, rounding the values to either 255 or 0. As this only works for monochromatic images, a second method which sets the least-signficiant bit to 0 is also included.

--- Project4/stegoDestroy.c
+++ Project4/stegoDestroy.c
@@ -1,12 +1,48 @@
-// Needed to continue using 'sprintf', although these should be replaced by 'sprintf_s' instead of silencing the warning.
+// Needed to continue using 'sprintf', although this should be replaced by 'sprintf_s' instead of silencing the warning.
 #define _CRT_SECURE_NO_WARNINGS
 
-#include<stdio.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include "stego.h"
 
+// Will remove any stego data from a character, even if that data is not actually stego data.
+char removeStego(char character, bool roundToNearest) {
+	if (roundToNearest) {
+		int characterInt = (int)character;
+
+		// Check if character is a positive number. If not, compute its positive value by taking the negative number and adding it to 265 (max + 1 of character).
+		if (character < 0) {
+			characterInt = 256 + character;
+		}
+
+		if (characterInt >= 254) {
+			return 255;
+		}
+		else {
+			return 0;
+		}
+	}
+
+	// If we don't need to attempt a repair, return the character with the least significant (furthest-right) bit masked out. This means that 255 will be equal to 254, while 0 stays the same.
+	//return character & 0b11111110;
+
+	// This will instead always set the final bit to 1 regardless of the value of the original image. Means that values greater than 254 will be equal to 255, while 0 will be equal to 1.
+	return character | 0b00000001;
+}
+
 int main(int argc, const char* argv[])
 {
+	// Controls whether detected stego headers, inserted bits, and stego data should be kept in the output file.
+	bool keepStegoHeader = false;
+	bool keepInsertedBits = false;
+	bool keepStegoData = false;
+
+	// Controls whether the program should exit if an invalid stego header is detected. Turning this to false will likely produce a malformed file.
+	bool exitOnInvalidStegoHeader = true;
+
+	// Whether rounding should be used for removing stego data. This is capable of producing the original image from an image with stego data only if the image is monochromatic, as it will round up values from 254 to 255 (inclusive) or round down values from 1 to 0 (inclusive).
+	bool roundToNearest = true;
+
 	if (argc != 3)
 	{
 		fprintf(stderr, "\nUsage: %s stegoImage outData\n\n", argv[0]);
@@ -15,36 +51,33 @@ int main(int argc, const char* argv[])
 		exit(0);
 	}
 
-	FILE* in;
-	FILE* outdata;
+	// Read both the inpput and output file names from the arguments passed to the program and attempt to open them, exiting the program if an error is encountered.
+	FILE* inputFile;
+	FILE* outputFile;
 
-	char infname[80];
-	size_t argv1Length = strlen(argv[1]);
-	sprintf(infname, argv[1]);
-	in = fopen(infname, "r");
-	if (in == NULL)
+	// This should really be taken from the system enviroment variables, as paths can be a lot longer than 255 in modern day.
+	char inputFileNmae[255];
+	sprintf(inputFileNmae, argv[1]);
+	inputFile = fopen(inputFileNmae, "r");
+	if (inputFile == NULL)
 	{
-		fprintf(stderr, "\nError opening file %s\n\n", infname);
+		fprintf(stderr, "\nError opening file %s\n\n", inputFileNmae);
 		exit(0);
 	}
 
-	char outdatafname[80];
-	size_t argv2Length = strlen(argv[2]);
-	sprintf(outdatafname, argv[2]);
-	outdata = fopen(outdatafname, "w");
-	if (outdata == NULL)
+	char outputFileName[255];
+	sprintf(outputFileName, argv[2]);
+	outputFile = fopen(outputFileName, "w");
+	if (outputFile == NULL)
 	{
-		fprintf(stderr, "\nError opening file %s\n\n", outdatafname);
+		fprintf(stderr, "\nError opening file %s\n\n", outputFileName);
 		exit(0);
 	}
 
-	char temp;
-	char data = 0;
+	// Initialize the variables required for the main logic loop.
+	char fileChar;
 
-	//fclose(in);
-	//in = fopen(infname, "r");
-
-	int shft = 0;
+	int shift = 0;
 
 	bool inStegoHeader = false;
 	int stegoHeaderIndex = 0;
@@ -58,30 +91,36 @@ int main(int argc, const char* argv[])
 
 	int dataBitsByteReplacement = 2;
 
-	int i = 0;
-	while (fscanf(in, "%c", &temp) == 1)
+	int fileCharIndex = 0;
+	// While an error is not returned from scanning the next char into 'fileChar'...
+	while (fscanf(inputFile, "%c", &fileChar) == 1)
 	{
-		if (i >= START_FROM && !inStegoHeader && stegoHeaderIndex == 0) {
+		// If the current file char index is greater than or equal to the start from and we are not in the stego header and we haven't read the stego header, then we are in the stego header.
+		if (fileCharIndex >= START_FROM && !inStegoHeader && stegoHeaderIndex == 0) {
 			inStegoHeader = true;
 		}
 
+		// If we are not in the stego header and we have read the stego header and we are not in the inserted bits index and we haven't read the inserted bits, then we are in the inserted bits section.
 		if (!inStegoHeader && stegoHeaderIndex != 0 && !inInsertedBits && insertedBitsIndex == 0) {
 			inInsertedBits = true;
 		}
 
+		// If we are in the inserted bits section, read 27 bits and remove if we are set to not keep the inserted bits.
 		if (inInsertedBits) {
 			insertedBitsIndex++;
 
-			// Zero out inserted bits.
-			//temp = insertedBitsByteReplacement;
+			if (!keepInsertedBits) {
+				fileChar = removeStego(fileChar, roundToNearest);
+			}
 
 			if (insertedBitsIndex >= 27) {
 				inInsertedBits = false;
 			}
 		}
 
+		// If we are in the stego header, check that the header data is valid using the same formula as stegoRead. If we are not supposed to keep the stego header, replace the bits with 0x0.
 		if (inStegoHeader) {
-			ttt ^= ((temp & 0x1) << j);
+			ttt ^= ((fileChar & 0x1) << j);
 
 			j++;
 			stegoHeaderIndex++;
@@ -91,10 +130,10 @@ int main(int argc, const char* argv[])
 				if (ttt != 0xa5)
 				{
 					fprintf(stderr, "\nError --- file does not contain stego data that I can read. ttt=%i\n\n", ttt);
-					exit(0);
-				}
-				else {
-					fprintf(stderr, "\nValid ttt value\n\n");
+
+					if (exitOnInvalidStegoHeader) {
+						exit(0);
+					}
 				}
 
 				ttt = 0;
@@ -104,39 +143,25 @@ int main(int argc, const char* argv[])
 				inStegoHeader = false;
 			}
 
-			// Zero out Stego header.
-			//temp = stegoHeaderByteReplacement;
+			if (!keepStegoHeader) {
+				fileChar = removeStego(fileChar, roundToNearest);
+			}
 		}
 
-		if (!inInsertedBits && insertedBitsIndex != 0 && !inStegoHeader && stegoHeaderIndex != 0) {
-			temp = temp & 0b11111110;
-
-			//data = data ^ ((temp & 0x1) << shft);
-			//++shft;
-			//if (shft == 8)
-			//{
-			//	//fprintf(stderr, "\n1: %i\n", temp);
-			//	//fprintf(stderr, "\n2: %i\n", data);
-
-			//	//temp = dataBitsByteReplacement;
-			//	//temp = temp ^ 0x0;
-
-			//	if (data > 0) {
-			//		temp = temp - data;
-			//	}
-
-			//	data = 0;
-			//	shft = 0;
-			//}
+		// If we have processed the inserted bits and stego header sections, we are in the actual data of the file. Remove the stego from the char.
+		if (!keepStegoData && !inInsertedBits && insertedBitsIndex != 0 && !inStegoHeader && stegoHeaderIndex != 0) {
+			fileChar = removeStego(fileChar, roundToNearest);
 		}
 
-		fprintf(outdata, "%c", temp);
+		// Print the (either modified or unmodified) char value into the output file.
+		fprintf(outputFile, "%c", fileChar);
 
-		i++;
+		fileCharIndex++;
 	}
 
 	printf("\n");
 
-	fclose(in);
-	fclose(outdata);
+	// Close both of the files.
+	fclose(inputFile);
+	fclose(outputFile);
 }
